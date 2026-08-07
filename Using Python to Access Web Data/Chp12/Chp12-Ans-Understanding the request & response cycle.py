60df025a40db2a34968e3cdffb8d2c5a27e73634
sadatkhan-codes <md.sadatkhan2023@gmail.com>
2026-08-07 22:27:55 +0530

    Chp12

--- Using Python to Access Web Data/Chp12/Chp12-Ans-Understanding the request & response cycle.py
+++ Using Python to Access Web Data/Chp12/Chp12-Ans-Understanding the request & response cycle.py
@@ -1,15 +1,21 @@
-import socket
+import socket     # Get Python's networking tools
 
+# Create a network connection
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the web server
 mysock.connect(('data.pr4e.org', 80))
 
+# Create a request saying "Give me intro-short.txt" and convert it to bytes.
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
+    data = mysock.recv(512)           # receive data upto 512 bytes
+    if len(data) < 1:                 # when there is 0 data then break the while loop
         break
-    print(data.decode(), end='')
+    print(data.decode(), end='')      
+    
+# convert bytes into python string and dont add new lines, the data we're receiving already contains its own line breaks.
 
 mysock.close()
